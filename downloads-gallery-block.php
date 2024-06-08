194cc607146c5d934b9e4e8c0b8b40d09ed56198
Max Gruson <max@gruson.studio>
2024-06-08 15:47:06 +0200

    Fix versioning

--- downloads-gallery-block.php
+++ downloads-gallery-block.php
@@ -4,7 +4,7 @@
  * Description:       A block to add a gallery with downloads. The data includes a name, a link, and a button text.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            <a href="https://max.gruson.studio" target="_blank">Max Gruson</a>
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
